d1991747cc930ab54494bbbd87f9fb63a29f4f0b
JyoshnaMP <jyoshna.d@moonpreneur.com>
2026-04-27 06:40:27 +0530

    Update mail.js

--- Utils/mail.js
+++ Utils/mail.js
@@ -1,19 +1,27 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{user:process.env.EMAIL,
-        pass: process.env.PASS
-    }
+  service:"gmail",
+  auth:{
+    user:process.env.EMAIL,
+    pass:process.env.PASS
+  }
 });
-const sendReminderMail = async(title, discription, priority, maindate)=>{
-    await transporter.sendMail({
-        from:process.env.EMAIL, to: process.env.EMAIL, 
-        subject:`reminder : ${title}`, 
-        text:`reminder alert
-        Title:${title}
-        Discription:${discription}
-        Priority:${priority}
-        Maindate:${maindate}`
-    });
+
+const sendReminderMail = async (title,description,priority,mainDate)=>{
+  await transporter.sendMail({
+    from:process.env.EMAIL,
+    to:process.env.EMAIL,
+    subject:`Reminder: ${title}`,
+    text:`
+Reminder Alert
+
+Title: ${title}
+Description: ${description}
+Priority: ${priority}
+Event Time: ${mainDate}
+    `
+  });
 };
+
 module.exports = sendReminderMail;
