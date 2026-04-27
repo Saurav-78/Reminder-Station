5f6b3bd7c54668f1ed90d21f8e1c17e7c9b1ee94
JyoshnaMP <jyoshna.d@moonpreneur.com>
2026-04-27 07:27:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const cron = require("node-cron");
-
-// dotenv.config();
-
-// const connectDB = require("./config/db");
-// const authRoutes = require("./routes/authRoutes");
-// const reminderRoutes = require("./routes/reminderRoutes");
-
-// const Reminder = require("./models/Reminder");
-// const sendReminderMail = require("./Utils/mailer");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// connectDB();
-
-// app.get("/",(req,res)=>{
-//   res.send("API Running");
-// });
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/reminder", reminderRoutes);
-
-// cron.schedule("* * * * *", async ()=>{
-
-//   const now = new Date();
-//   const reminders = await Reminder.find({completed:false});
-
-//   for(const r of reminders){
-
-//     for(const rDate of r.reminderDates){
-
-//       const reminderTime = new Date(rDate);
-
-//       const alreadySent = r.sentReminderDates.some(
-//         d => new Date(d).getTime() === reminderTime.getTime()
-//       );
-
-//       if(alreadySent) continue;
-
-//       if(now >= reminderTime){
-
-//         await sendReminderMail(
-//           r.title,
-//           r.description,
-//           r.priority,
-//           r.mainDate
-//         );
-
-//         r.sentReminderDates.push(reminderTime);
-//         await r.save();
-//       }
-//     }
-//   }
-
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT,()=>{
-//   console.log("Server running on",PORT);
-// });
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -76,6 +8,7 @@ const connectDB = require('./Config/DB');
 const authroutes = require('./routes/authRoutes');
 const reminderroutes = require('./Routes/reminderroutes');
 const Reminder = require('./models/Reminder');
+const User = require('./models/user');
 const sendReminderMail = require('./Utils/mail');
 
 const app = express();
@@ -112,7 +45,10 @@ cron.schedule("* * * * *", async () => {
 
                 if (now >= reminderTime) {
                     console.log(`📧 Sending email for: "${r.title}"`);
-                    await sendReminderMail(r.title, r.description, r.priority, r.maindate);
+                    const user = await User.findById(r.userId);
+                    if (user) {
+                        await sendReminderMail(r.title, r.description, r.priority, r.maindate, user.email);
+                    }
                     r.sendReminderDates.push(reminderTime);
                     await r.save();
                     console.log(`✅ Email sent for: "${r.title}"`);
