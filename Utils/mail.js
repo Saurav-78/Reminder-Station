b6a1c8fb358d3929f4c9f903c7c15328bbc852bb
Saurav-78 <saurav.pandit024@gmail.com>
2026-05-03 18:49:09 -0700

    Update email password environment variable

--- Utils/mail.js
+++ Utils/mail.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.PASS
     }
 });
 
